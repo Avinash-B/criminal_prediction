bbc0b61f1024dd3f9381565a208560c4e5918b4f
Avinash-B <boppudiavinash@gmail.com>
2018-02-14 15:07:54 +0530

    Problem with list sent over function

--- classifier.py
+++ classifier.py
@@ -15,37 +15,42 @@ import numpy as np
 from sklearn import tree
 
 class classifier:
-	def __init__(self,keyls,trainlist):
-		self.keyls = keyls
+	def __init__(self,trainlist):
 		self.trainlist = trainlist
-		pass
-	def test(self,element):
+	def test(self,ls):
 		result = 0
 		for i in range(0,2001):
 			#30 for random subset from the dataset
+			treelist = []
 			treelist = random.sample(self.trainlist,30)
-			print treelist
-			return result
-
+			treeresult = []
+			for one in treelist:
+				one.pop(0)
+				treeresult.append(one.pop())
+				for every in one:
+					every = float(every)
+			for every in treeresult:
+				every = int(every)
+			clf = tree.DecisionTreeClassifier()
+			clf = clf.fit(treelist,treeresult)
+			result+= clf.predict(ls)
+		return result
 
 
 if __name__ == "__main__":
-	with open('criminal_train.csv') as keyfile:
-		keylist = csv.DictReader(keyfile)
-		for row in keylist:
-			keyls = row.keys()
-			break
 	with open('criminal_train.csv') as trainfile:
 		traindata = csv.reader(trainfile)
 		trainlist = list(traindata)
-		forest = classifier(keyls,trainlist)
+		forest = classifier(trainlist)
 		with open('criminal_test.csv') as testfile:
 			testdata = csv.reader(testfile)
 			testlist = list(testdata)
 			for element in testlist:
-				result = forest.test(element)
-				if result>1000:
-					print str(element[0])+" "+str(1)
-				else:
-					print str(element[0])+" "+str(0)
+				PERID = element.pop(0)
+				ls = list(element)
+				result = forest.test(ls)
+				# if result>1000:
+				# 	print str(PERID)+" "+str(1)
+				# else:
+				# 	print str(PERID)+" "+str(0)
 	# with open(result.csv) as resultfile:
